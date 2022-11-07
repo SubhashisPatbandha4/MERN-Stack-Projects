142682b98c0b1cafa144af34fe069720e82fa25c
subhashis patbandha <subhashispatbandha4@gmail.com>
2022-11-07 23:08:58 +0530

    contact page frontend added

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -5,24 +5,77 @@ const Contact = () => {
     <>
       <div className="contact-info">
         <div className="container-fluid">
-          <div className="row mx-auto">
-            <div className="col-lg-10">
-              <div className="contact-info-item d-flex justify-content-start align-items-center" >
-              <i class="fa-solid fa-phone-volume"></i>
-                <div className="contact-info-contact">
-                  <div className="contact-info-title">
-                    phone
-                  </div>
-                  <div className="contact-info-text">
-                    1234567890
-                  </div>
+
+          <div className="col-lg-12 d-flex row">
+            <div className="contact-info-item d-flex justify-content-start align-items-center shadow col-sm-3" >
+              <img style={{ height: "30px" }} src="https://img.freepik.com/free-vector/human-hand-holding-mobile-phone-with-text-messages_74855-6531.jpg?size=626&ext=jpg&uid=R81755913" alt="phone" />
+              <div className="contact-info-contact">
+                <div className="contact-info-title">
+                  phone
+                </div>
+                <div className="contact-info-text">
+                  1234567890
                 </div>
               </div>
+            </div>
+            <div className="contact-info-item d-flex justify-content-start align-items-center shadow col-sm-3" >
+              <img style={{ height: "30px" }} src="https://img.freepik.com/free-vector/mail-write-get-send-icons-vector-illustration-flat-style_1284-42663.jpg?w=740&t=st=1667839042~exp=1667839642~hmac=8cc388b9906c7d717a579d6dfff01fb309765d16d78a502077a05f3108a5c4f3" alt="phone" />
+              <div className="contact-info-contact">
+                <div className="contact-info-title">
+                  Email
+                </div>
+                <div className="contact-info-text">
+                  subh@gmail.com
+                </div>
+              </div>
+            </div>
+            <div className="contact-info-item d-flex justify-content-start align-items-center shadow col-sm-3" >
+              <img style={{ height: "30px" }} src="https://img.freepik.com/free-vector/location-based-promotion-geolocation-software-online-gps-app-navigation-system-geographic-restriction-man-searching-address-with-magnifier-concept-illustration_335657-2082.jpg?w=740&t=st=1667839479~exp=1667840079~hmac=04ec8ebc6bb99d0949d8667bfafe5ed30095dc95a25af401673ba246db31ea6e" alt="phone" />
+              <div className="contact-info-contact">
+                <div className="contact-info-title">
+                  Address
+                </div>
+                <div className="contact-info-text">
+                  bbsr
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+      {/* contact us form */}
+      <div className="contact-form">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-10 offset-lg-1 shadow">
+              <div className="contact-form-container py-5">
+                <div className="contact-form-title text-center">
+                  Contact Us
+                </div>
+                <form id="contact-form">
+                  <div className="contact-form-content row mx-auto d-flex justify-content-between ">
+                    <input type="text" id="contact-form-name" className="contact-form-name input-field col-sm-3 " placeholder="your name" required='true' />
+                    <input type="email" id="contact-form-email" className="contact-form-email input-field col-sm-3" placeholder="your email" required='true' />
+                    <input type="number" id="contact-form-number" className="contact-form-number input-field col-sm-3" placeholder="your number" required='true' />
+
+                    <div className="contact-form-text mt-5 col-sm-12">
+                      <textarea name="textarea" id="" className="col-sm-12" placeholder="Message" cols="30" rows="10" ></textarea>
+                    </div>
+                    <div className="contact-form-btn">
+                      <button type="submit" className="btn btn-primary p-2">Send Message</button>
+                    </div>
+                  </div>
+                </form>
+              </div>
+
+
             </div>
           </div>
         </div>
       </div>
     </>
+
   );
 };
 
